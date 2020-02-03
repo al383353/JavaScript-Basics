b48953aa3573206f5967bafddabcd78b6ee010ef
senait <al383353@gmail.com>
2020-02-03 16:54:25 +0100

    have repo

---
+++ basics.js
@@ -0,0 +1,10 @@
+console.log("Hello world!");
+let person = {
+    name: 'Senait',
+    age: 26
+};
+person.name= 'Miki';
+console.log(person)
+
+var age = prompt("What is your age?");
+document.getElementById("age").innerHTML = age;
--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello world!");
-let person = {
-    name: 'Senait',
-    age: 26
-};
-person.name= 'Miki';
-console.log(person)
