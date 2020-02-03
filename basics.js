71c57ff353bc6cd7e759e1dfdbd638fff89bfd1e
senait <al383353@gmail.com>
2020-02-03 17:56:19 +0100

    till 1:37:35

--- basics.js
+++ basics.js
@@ -4,7 +4,41 @@ let person = {
     age: 26
 };
 person.name= 'Miki';
-console.log(person)
+console.log(person.name);
 
-var age = prompt("What is your age?");
-document.getElementById("age").innerHTML = age;
+//var age = prompt("What is your age?");
+document.getElementById("age").innerHTML = person.age;
+
+//Data types
+let urAge = 28; let name = 'Senait'; 
+let fullName = {first: 'Senait Meles', last: 'Alema'};
+let truth = true;
+let groceries = ['apple', 'banana', 'oranges'];
+console.log(groceries.length);
+for(let gr = 0; gr < groceries.length; gr++){
+    document.getElementById("groceries").innerHTML = groceries[gr];
+ }
+
+// Switch statements of weekdays
+//day 0 is Sunday, day 6 is Saturday, day 4 is Thursday --> weekday
+
+switch(2) {
+    case 0:
+        text = 'Weekend';
+        break;
+    case 5:
+        text = 'weekend';
+        break;
+    case 6:
+        text = 'weekend';
+        break;
+    default:
+        text = 'Weekday';
+    
+}
+console.log(text);
+
+//JSON/represents data, In JSan array with objects inside of it
+//JS object notation, In python called list with dictionaries
+
+//https://www.youtube.com/watch?v=Qqx_wzMmFeA 1:37:35
